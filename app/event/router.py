7a058c483846df497241eaf00cb4b2222dcc8ea1
model0627 <model0627@gmail.com>
2024-04-19 15:35:54 +0900

    auth add

--- app/event/router.py
+++ app/event/router.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from typing import Annotated, Any, Literal
 
 from fastapi import Depends, APIRouter, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, APIKeyHeader
+from fastapi.security import OAuth2PasswordBearer, APIKeyHeader, OAuth2PasswordRequestForm
 
 from pydantic import BaseModel
 from jose import ExpiredSignatureError, JWTError, jwt
@@ -104,13 +104,83 @@ async def protected_route(token: str = Depends(api_key_header)):
     if token != API_TOKEN:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     
+    print("Hello")
     encoded_data = create_access_token(encode_token)
 
     return encoded_data
 
 
-@app.get("oauth/login")
-async def protected_route(token: Annotated[str, Depends(oauth2_scheme)]):
-    
 
-    raise CREDENTIALS_EXCEPTION
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+class User(BaseModel):
+    username: str
+    password: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+
+@app.post("oauth/login")
+async def login(user: User, token: str = Depends(api_key_header)):
+    if token != API_TOKEN:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    print(user)
+    # user_dict = fake_users_db.get(form_data.username)
+    # print(user_dict)
+    # if not (user.username):
+    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
+    # user = UserInDB(**user_dict)
+    # print(user)
+    # hashed_password = fake_hash_password(form_data.password)
+    # print(hashed_password, user.hashed_password)
+    # if not hashed_password == user.hashed_password:
+    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
